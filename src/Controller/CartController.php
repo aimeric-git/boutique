55201650f40df72bf2a7d63add9688be04c79452
Aimeric <aimericjjb@gmail.com>
2021-08-25 14:38:56 +0300

    add a button to decrease and increase the quantity of product, refactor the method index in CartController, and add a security when add product in pannier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -11,30 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    private $entityManager; 
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager; 
-    }
-
+  
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        $cartComplete = [];
-
-        foreach ($cart->get() as $id => $quantity)
-        {
-            $cartComplete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' => $quantity
-            ];
-        }
-
         return $this->render('cart/index.html.twig', [
-            'cart' =>  $cartComplete
+            'cart' =>  $cart->getFull()
         ]);
     }
 
@@ -54,5 +38,23 @@ class CartController extends AbstractController
     {
         $cart->remove();
         return $this->redirectToRoute('home'); 
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete_to_cart")
+     */
+    public function delete(Cart $cart, $id)
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('cart'); 
+    }
+
+     /**
+     * @Route("/cart/decrease/{id}", name="decrease_cart")
+     */
+    public function decrease(Cart $cart, $id)
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart'); 
     }
 }
