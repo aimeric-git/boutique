e6f756dc35c347ec530282ec014d7748c31c52c8
Aimeric <aimericjjb@gmail.com>
2021-08-30 19:45:58 +0300

    display button to validate the order and to select a carrier

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Address;
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +14,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/commande", name="order")
      */
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if(!$this->getUser()->getAddresses()->getValues() )
         {
@@ -23,8 +24,15 @@ class OrderController extends AbstractController
             'user' => $this->getUser()
         ]);
 
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            
+        }
+
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(), 
+            'cart' => $cart->getFull()
         ]);
     }
 }
