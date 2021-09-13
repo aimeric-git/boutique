5e9f1b16dd8d052998ec2ee5f1b2923189dbf720
Aimeric <aimericjjb@gmail.com>
2021-09-13 12:10:25 +0300

    create entity Header,create header in backoffice and display the header in homapage

---
+++ src/Controller/Admin/HeaderCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Header;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class HeaderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Header::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+           TextField::new('title', 'titre du Header'),
+           TextareaField::new('content', 'contenu'), 
+           TextField::new('btnTitle', 'titre du bouton'), 
+           TextField::new('btnUrl', 'url de notre destination'), 
+           ImageField::new('illustration')
+                        ->setBasePath('uploads/')
+                        ->setUploadDir('public/uploads')
+                        ->setUploadedFileNamePattern('[randomhash].[extension]')
+                        ->setRequired(false)
+        ];
+    }
+}
